245d5ab3e48d004887c4d3fd92b20e20a4255dc0
georgehynes <info@techybyte.co.uk>
2022-11-08 20:02:28 +0000

    Encapsulation with nested classes

---
+++ ECM2414/DeckCollection.java
@@ -0,0 +1,38 @@
+package ECM2414;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeckCollection {
+    class Deck {
+        class Card {
+            private final int value;
+
+            Card(int value){
+                this.value = value;
+            }
+
+            public int getValue() {
+                return value;
+            }
+        }
+        List<Card> cards = new ArrayList<>();
+        public Card draw() {
+            return cards.remove(0);
+        }
+        public void discard(Card card) {
+            cards.add(card);
+        }
+    }
+    List<Deck> decks = new ArrayList<>();
+    public Deck.Card draw(int playerNumber) {
+        return decks.get(playerNumber).draw();
+    }
+    public void discard(int playerNumber, Deck.Card card) {
+        if (playerNumber == decks.size()) {
+            decks.get(0).discard(card);
+        } else {
+            decks.get(playerNumber+1).discard(card);
+        }
+    }
+}
--- ECM2414/Player.java
+++ ECM2414/Player.java
@@ -1,14 +1,27 @@
 package ECM2414;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Player {
-    List<Card> hand = new ArrayList<>();
-    private int playerNumber;
+    List<DeckCollection.Deck.Card> hand = new ArrayList<>();
+    private final int playerNumber;
 
-    Player(int playerNumber){
+    Player(int playerNumber) {
         this.playerNumber = playerNumber;
     }
-    
+
+    public void receive(DeckCollection.Deck.Card c) {
+        hand.add(c);
+    }
+
+    public synchronized void main() {
+
+    }
+
+    private boolean checkIfWon() {
+        return hand.isEmpty() || Collections.frequency(hand, hand.get(0)) == hand.size();
+    }
+
 }
